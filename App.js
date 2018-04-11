6313533d602ef1ea3d0fc8658cadd498313a75ff
Hunkyo Jung <hunkyo.jung@gmail.com>
2018-04-11 22:21:54 +0900

    Add basic template

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * @flow
+ */
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          HelloWorld Template
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
